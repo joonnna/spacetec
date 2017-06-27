b9625f8550f177dc56a16f8e0510f115e81e2993
Jon Foss Mikalsen <jon_fossen94@hotmail.com>
2017-06-27 08:43:14 +0000

    Udp progress, started parsing

--- statemachine/comm.py
+++ statemachine/comm.py
@@ -8,20 +8,32 @@ class Communication():
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((ip, port))
 
+        self.long_start = 171
+        self.long_end = 180
+        self.lat_start = 182
+        self.lat_end = 190
+        self.height_start = 192
+        self.height_end = 198
+
 
     def receive_data(self):
         data, addr = self.socket.recvfrom(500)
-        print "yoyooy got this : ", data
         return data
 
     def calc_pos(self, data):
-        pass
+        longtitude = float(data[self.long_start:self.long_end])
+        latitude = float(data[self.lat_start:self.lat_end])
+        height = float(data[self.height_start:self.height_end])
+        print longtitude, latitude, height
 
     def send_pos(self, pos):
         pass
 
     def run(self):
+        packages = 0
         while True:
             data = self.receive_data()
+            packages += 1
+            print packages
             pos = self.calc_pos(data)
             self.send_pos(pos)
